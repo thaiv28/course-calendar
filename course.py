a4103b8a5a443b31ae066b949253e8db69ab0114
Thai Villaluna <thaivillaluna@gmail.com>
2023-11-25 15:52:29 -0800

    feat: parse pdf for code, dates, time

---
+++ course.py
@@ -0,0 +1,12 @@
+class Course():
+    
+    def __init__(self, code, comp, days, times, room, start, end):
+        self.code = code
+        self.comp = comp
+        self.days = days
+        self.times = times
+        self.room = room
+        self.start = start
+        self.end = end
+        
+    
