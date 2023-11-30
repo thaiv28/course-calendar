0ee85bc877389229bfb838cd089d342b801b4635
Thai Villaluna <thaivillaluna@gmail.com>
2023-11-29 17:16:04 -0800

    clean up main.py

--- src/main.py
+++ src/main.py
@@ -43,32 +43,11 @@ def connect():
 
 	# Call the Calendar API
 		now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-	# print("Getting the upcoming 10 events")
-	# events_result = (
-	#     service.events()
-	#     .list(
-	#         calendarId="primary",
-	#         timeMin=now,
-	#         maxResults=10,
-	#         singleEvents=True,
-	#         orderBy="startTime",
-	#     )
-	#     .execute()
-	# )
-	# events = events_result.get("items", [])
-
-	# if not events:
-	#   print("No upcoming events found.")
-	#   return
-
-	# # Prints the start and name of the next 10 events
-	# for event in events:
-	#   start = event["start"].get("dateTime", event["start"].get("date"))
-	#   print(start, event["summary"])
 		return service
 
 	except HttpError as error:
 		print(f"An error occurred: {error}")
+  
   
 def get_calendars(service):
 	cal_list = service.calendarList().list().execute()
@@ -96,10 +75,8 @@ def main():
   
 	for e in e_list:
 		if e != None:
-			
 			e = service.events().insert(calendarId=cal_dict["API test"], body=e).execute()
-  
-	print('Event created: %s' % (e.get('htmlLink')))
+			print('Event created: %s' % (e.get('htmlLink')))
 	
 
 
